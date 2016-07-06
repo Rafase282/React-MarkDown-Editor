741d3444bd3518e2ebf9d44256ab4efad753f124
Rafael J. Rodriguez <rafase282@gmail.com>
2016-07-06 12:37:58 -0400

    Update react

--- js/index.js
+++ js/index.js
@@ -4,14 +4,22 @@ var MarkdownPreview = React.createClass({
   displayName: "MarkdownPreview",
 
   getInitialState: function getInitialState() {
-    return { content: "Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes * italic *, ** bold **,\n `monospace`, ~~ strikethrough ~~.\n\nShopping list : \n\n * apples\n * oranges\n * pears\n\nNumbered list : \n\n 1. apples\n 2. oranges\n 3. pears\n\nThe rain-- -not the reign-- -in\nSpain.\n\n * [Rafael Rodriguez](http : //freecodecamp.com/Rafase282)*\n" };
+    return {
+      content: "Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes * italic *, ** bold **,\n `monospace`, ~~ strikethrough ~~.\n\nShopping list : \n\n * apples\n * oranges\n * pears\n\nNumbered list : \n\n 1. apples\n 2. oranges\n 3. pears\n\nThe rain-- -not the reign-- -in\nSpain.\n\n * [Rafael Rodriguez](http : //freecodecamp.com/Rafase282)*\n"
+    };
   },
   onChange: function onChange(e) {
-    this.setState({ content: e.target.value });
+    this.setState({
+      content: e.target.value
+    });
   },
   rawMarkup: function rawMarkup() {
-    var rawMarkup = marked(this.state.content, { sanitize: true });
-    return { __html: rawMarkup };
+    var rawMarkup = marked(this.state.content, {
+      sanitize: true
+    });
+    return {
+      __html: rawMarkup
+    };
   },
   render: function render() {
     return React.createElement(
@@ -33,11 +41,8 @@ var MarkdownPreview = React.createClass({
             { className: "title" },
             "Markdown"
           ),
-          React.createElement(
-            "textarea",
-            { className: "mark", onChange: this.onChange },
-            this.state.content
-          )
+          React.createElement("textarea", { className: "mark", value: this.state.content,
+            onChange: this.onChange })
         ),
         React.createElement(
           "div",
